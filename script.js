f53be793b997afcb432599821b6c9e0a38063214
unsortedAndy23 <unsortedandy@gmail.com>
2026-04-05 07:49:24 +0530

    basic layout & proper random bmb generation

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const columns = 10; //number of tiles vertically
 const density = 0.15;
 const totalBombs = Math.floor(rows*columns*density); // Greatest Integer Function
 
+const board = document.getElementById('board');
+
 let tileSheet = []; // empty cartesian coordinate system array.
 let bombSheet = [];
 
@@ -20,13 +22,30 @@ for (let y=1; y < columns+1; y++){
 
 }
 
+function bombPlacer(){
+//ensures no duplicates
+        let axe = Math.floor(Math.random()*rows) +1;
+        let why = Math.floor(Math.random()*columns) +1;
+        if(bombSheet.some(t => t.x === axe && t.y === why)) return bombPlacer();
+        else return {axe, why};
+}
+
 // choosing bomb spots
     for(let i=1; i < totalBombs+1; i++){
+        let {axe, why} = bombPlacer();
         bombSheet.push({
-            x: Math.round(Math.random()*rows),
-            y: Math.round(Math.random()*columns)
+            x: axe,
+            y: why
         })
     }
 
-
-console.log(bombSheet);
+// Adding divs
+tileSheet.forEach((tile) => {
+    const tempTile = document.createElement('div');
+    tempTile.className = 'tile';
+    tempTile.style.top = `${-40 + tile.x*40}px`;
+    tempTile.style.left = `${-40 + tile.y*40}px`;
+    tempTile.innerText = `${tile.x}, ${tile.y}`;
+    if(bombSheet.some(t => t.x === tile.x && t.y === tile.y)) tempTile.classList.add('bomb');
+    board.appendChild(tempTile)
+})
